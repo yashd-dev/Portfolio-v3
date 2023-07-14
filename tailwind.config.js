eadebbf9af979e80702c80c71c9a2e6b0811bfdf
Yash <78804783+yashd-dev@users.noreply.github.com>
2023-07-14 16:11:58 +0530

    Rearranged tailwind components in right order

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/*.html", "./src/notes/rps.html"],
-	theme: {
-		extend: {
-			fontFamily: {
-				robotomono: ["JetBrains Mono", "monospace"],
-			},
-		},
-	},
-	plugins: [require("@tailwindcss/typography")],
+  content: ["./src/*.html", "./src/notes/rps.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        robotomono: ["JetBrains Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
